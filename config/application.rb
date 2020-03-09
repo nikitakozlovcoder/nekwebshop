be131c7bfeb75944b920cb66fd5203559f7d4687
unknown <nikiton2606@mail.ru>
2020-03-09 21:28:32 +0300

    init

--- config/application.rb
+++ config/application.rb
@@ -1,20 +1,11 @@
 require_relative 'boot'
 
-require "rails"
+require "rails/all"
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
+
 require "sprockets/railtie"
-require "rails/test_unit/railtie"
-config.assets.initialize_on_precompile = false
+
+#
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -23,7 +14,7 @@ module Nekwebshop
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
