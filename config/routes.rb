640e71046ad5731b221441bd6f15057ee4c4a517
nikitakozlovcoder <nikiton2606@mail.ru>
2020-04-14 16:38:58 +0300

    init

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'products', to:'products#index'
   get 'blog', to:'posts#index'
   get 'contact', to:'home#contact'
+  get 'profile', to:'users#profile'
   resources 'cart', only: [:index, :new], path_names: {new: 'checkout'}
 end
