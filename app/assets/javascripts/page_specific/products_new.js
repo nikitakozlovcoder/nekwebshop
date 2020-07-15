8b6c5ddbdbf8c844eebf5813e9b21235937690b6
Nikita Zhukovskiy <zhukovskiy.sfedu@gmail.com>
2020-07-15 14:30:43 +0300

    Developer choosing option

--- app/assets/javascripts/page_specific/products_new.js
+++ app/assets/javascripts/page_specific/products_new.js
@@ -1,7 +1,15 @@
 let body = document.querySelector('body');
 if (body.classList.contains('products') && body.classList.contains('new'))
 {
-
+	function ListCheck(that) {
+		if (that.value == "Another") {
+			document.getElementById("developer_input").style.display = "block";
+			document.getElementById("developer_input_inp").value = "";
+		}
+		else {
+			document.getElementById("developer_input").style.display = "none";
+		}
+	}
     //TODO preview image
     //TODO generate form filed from json
 
