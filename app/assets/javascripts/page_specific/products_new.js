621d1e7f84d3b19c68685e38228a994593198a90
nikitakozlovcoder <nikiton2606@mail.ru>
2020-07-19 11:19:07 +0300

    hz

--- app/assets/javascripts/page_specific/products_new.js
+++ app/assets/javascripts/page_specific/products_new.js
@@ -1,14 +1,115 @@
+
 let body = document.querySelector('body');
 if (body.classList.contains('products') && body.classList.contains('new'))
 {
+	let another = -1;
+	function generateForm(data) {
+		let dev = document.querySelector('.form-group_dev');
+		let dev_select = document.querySelector('.form-group_dev_select');
+		dev.style.display = "block";
+		dev_select.innerHTML = "";
 
-    //TODO preview image
+
+
+		data.makers.forEach((el)=>{
+			dev_select.innerHTML += `<option value='${el.id}'>${el.name}</option>`
+			if (el.name=="Другое")
+			{
+				another = el.id;
+			}
+
+		});
+
+		$('select').niceSelect('update');
+
+		let generated_fields = document.querySelector(".generated_fields");
+		generated_fields.innerHTML="";
+
+
+		let fields = JSON.parse(data.category.data);
+		console.log("fields");
+		console.log(fields);
+		fields.forEach((el)=>{
+			let type="";
+			let text_to_add="";
+			if (el.type == "Number")
+			{
+				type = "number";
+				text_to_add=`<div class="form-group"><label>${el.name}<span>*</span></label><input name="${el.id}" type="${type}" placeholder="" required> </div>`;
+			}
+			else if(el.type == "Text")
+			{
+				type = "text";
+				text_to_add=`<div class="form-group"><label>${el.name}<span>*</span></label><input name="${el.id}" type="${type}" placeholder="" required></div>`;
+			}
+			else if(el.type == "LongText")
+			{
+				text_to_add=`<div class="form-group"><label>${el.name}<span>*</span></label><textarea name="${el.id}" type="${type}" placeholder="" required></textarea></div>`;
+			}
+			else if(el.type == "Bool")
+			{
+				type = "checkbox";
+				style = `width: 25px; height: 25px; display: block`;
+				text_to_add=`<div class="form-group"><label>${el.name}<span>*</span></label><input name="${el.id}" type="${type}" placeholder="" style="${style}" required></div>`;
+			}
+			else if(el.type == "Images")
+			{
+				style ="";
+				type="file";
+				text_to_add=`<div class="form-group"><label>${el.name}<span>*</span></label><input name="${el.id}" type="${type}" placeholder="" style="${style}" multiple></div>`;
+			}
+			generated_fields.innerHTML+=text_to_add;
+		})
+	}
+	function ListCheck(that) {
+
+		if (that.value == another) {
+			document.getElementById("developer_input").style.display = "block";
+			document.getElementById("developer_input_inp").value = "";
+		}
+		else {
+			document.getElementById("developer_input").style.display = "none";
+		}
+	}
+
+
+	function preview_image(event)
+	{
+		var reader = new FileReader();
+		reader.onload = function()
+		{
+		var output = document.getElementById('output_image');
+		output.src = reader.result;
+		output.style.display = "block";
+		}
+		reader.readAsDataURL(event.target.files[0]);
+	}
+
+	function CategoryCheck(e) {
+		let dev = document.querySelector('#developer_input');
+		let dev_inp = document.querySelector('#developer_input_inp');
+		dev.style.display = "none";
+		dev_inp.value = "";
+		fetch('/category/load/'+e.value)
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				console.log(data);
+				generateForm(data);
+
+			});
+
+
+
+	}
+
+
     //TODO generate form filed from json
 
 
 
 
 
-
-
 }
+
