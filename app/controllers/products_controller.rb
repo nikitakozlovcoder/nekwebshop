cda2f9d5a666f5fc620f861d74a6048749590d0b
nikitakozlovcoder <nikiton2606@mail.ru>
2020-08-12 15:41:53 +0300

    filters

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -52,7 +52,12 @@ class ProductsController < ApplicationController
     if params['makers']
       query << 'products.maker_id in (' + params[:makers].join(', ') + ')'
     end
-
+    if !params['price_min'].blank?
+      query << "products.price >='#{params['price_min']}'"
+    end
+    if !params['price_max'].blank?
+     query << "products.price <= '#{params['price_max']}'"
+    end
     order = :title
     if params[:order] && params[:order] == 'price'
       order = :price
@@ -62,15 +67,35 @@ class ProductsController < ApplicationController
     if @category and @category.is_template
       @template = JSON.parse @category.data
     end
-
+    query2 = []
     @template.select{|el| el['type'] == "Number"}.each do |el|
-      if !params[el['id'].to_s+"_min"].blank?
-          query << "(attributes.code = '#{el['id']}' and attributes.num >= '#{params[el['id'].to_s+"_min"]}')"
+      if !params[el['id'].to_s+"_min"].blank? && !params[el['id'].to_s+"_max"].blank?
+        query2 << "(attributes.code = '#{el['id']}' and attributes.num BETWEEN '#{params[el['id'].to_s+"_min"]}' and '#{params[el['id'].to_s+"_max"]}')"
+      elsif !params[el['id'].to_s+"_min"].blank?
+        query2 << "(attributes.code = '#{el['id']}' and attributes.num >= '#{params[el['id'].to_s+"_min"]}')"
+      elsif !params[el['id'].to_s+"_max"].blank?
+        query2 << "(attributes.code = '#{el['id']}' and attributes.num <= '#{params[el['id'].to_s+"_max"]}')"
       end
     end
     query = query.join ' and '
+    sum = query2.count
+    query2 = query2.join ' or '
     pp query
-    @products = Product.joins(:fields).where(query).order(order).distinct
+    if sum == 0
+      @products = Product.where(query).order(order).distinct
+    else
+      order_by = "ORDER BY products.title ASC"
+
+      if params['order'] == 'price'
+        order_by = "ORDER BY products.price ASC"
+      end
+      @products = Product.find_by_sql("
+       SELECT DISTINCT * FROM products WHERE id IN (SELECT attr.product_id FROM
+        (
+          SELECT COUNT(id) AS QTY, product_id FROM attributes WHERE (#{query2}) GROUP BY product_id
+        ) AS attr WHERE (attr.QTY = #{sum} and #{query})) #{order_by}
+      ")
+    end
 
 
     if @products.count > 0
