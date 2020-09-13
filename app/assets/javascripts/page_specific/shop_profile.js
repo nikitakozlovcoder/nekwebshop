0c4eb0bf7ca987e30cc4dad247a2e63acc1ba2af
Rodion Davidenko <rodrom.da@gmail.com>
2020-09-13 18:52:51 +0300

    fixed orders

--- app/assets/javascripts/page_specific/shop_profile.js
+++ app/assets/javascripts/page_specific/shop_profile.js
@@ -24,16 +24,25 @@ $(document).on('turbolinks:load', function () {
 
         //order menu functionality
         ShowOrder = function(el){
-            el.parentNode.classList.toggle('active');
+            let container = el.parentNode; 
+            container.classList.toggle('active');
             let arrow_list = el.querySelector('.inf_btn i').classList;
             if(arrow_list.contains('ti-angle-down')){
                 arrow_list.remove('ti-angle-down');
                 arrow_list.add('ti-angle-up');
+                container.querySelector('.inner_info').style.maxHeight = container.querySelector('.wrapper').clientHeight + "px";
             } else{
                 arrow_list.remove('ti-angle-up');
                 arrow_list.add('ti-angle-down');
+                container.querySelector('.inner_info').style.maxHeight = "0px";
             }
         }
+
+        window.addEventListener('resize', function() {
+            document.querySelectorAll('.order.active').forEach((el)=>{
+                el.querySelector('.inner_info').style.maxHeight = el.querySelector('.wrapper').clientHeight + "px";
+            });
+        });
 
         
     }
