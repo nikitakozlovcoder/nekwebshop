0c1c38fe5467d4cfb8d96d7703185139f759612f
nikitakozlovcoder <nikiton2606@mail.ru>
2020-10-14 14:23:56 +0300

    cloud storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,14 @@
 Rails.application.configure do
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            'nekmarketplace@gmail.com',
+      password:             'eeulqshtzdqxqgze',
+      authentication:       'plain',
+      enable_starttls_auto: true }
   #config.assets.css_compressor = :yui
   config.assets.js_compressor = Uglifier.new(:harmony => true)
   # Settings specified here will take precedence over those in config/application.rb.
