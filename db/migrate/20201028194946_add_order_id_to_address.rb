713eca928b03533befc008b2c03b8a8ce7ed8f5e
nikitakozlovcoder <nikiton2606@mail.ru>
2020-10-29 12:17:35 +0300

    fix

--- db/migrate/20201028194946_add_order_id_to_address.rb
+++ db/migrate/20201028194946_add_order_id_to_address.rb
@@ -1,18 +1,18 @@
 class AddOrderIdToAddress < ActiveRecord::Migration[6.0]
 
   def change
-    create_table :addresses do |t|
-      t.string :city
-      t.string :street
-      t.string :state
-      t.string :country
-      t.string :zip
-      t.string :suburb
-      t.string :county
-      t.integer :shop_id
+    #create_table :addresses do |t|
+      #t.string :city
+     # t.string :street
+     # t.string :state
+     # t.string :country
+     # t.string :zip
+     # t.string :suburb
+      #t.string :county
+     # t.integer :shop_id
 
-      t.timestamps
-    end
+     # t.timestamps
+   # end
     add_column :addresses, :order_id, :integer
   end
 end
